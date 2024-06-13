03ada877524627011626f577e776121d9c76dcf2
Aliff Ezaqiel Zaffrulla <aezaqiel@gmail.com>
2024-06-13 20:37:35 +0800

    lsp

--- lua/option.lua
+++ lua/option.lua
@@ -18,6 +18,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
+vim.opt.clipboard = "unnamedplus"
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
